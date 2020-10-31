168ea0f9450d4b2ed08502fc07f4c2385bab1103
binayakd <binayakd86@gmail.com>
2020-10-31 15:37:54 +0800

    initial commit - WIP

---
+++ pages/index.js
@@ -0,0 +1,72 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// react components for routing our app without refresh
+import Link from "next/link";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+// @material-ui/icons
+// core components
+import Header from "components/Header/Header.js";
+import HeaderLinks from "components/Header/HeaderLinks.js";
+import Footer from "components/Footer/Footer.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import Button from "components/CustomButtons/Button.js";
+import Parallax from "components/Parallax/Parallax.js";
+// sections for this page
+import SectionCompletedExamples from "pages-sections/Components-Sections/SectionCompletedExamples.js";
+import SectionLogin from "pages-sections/Components-Sections/SectionLogin.js";
+import SectionExamples from "pages-sections/Components-Sections/SectionExamples.js";
+import SectionDownload from "pages-sections/Components-Sections/SectionDownload.js";
+
+import styles from "assets/jss/pages/index.js";
+
+const useStyles = makeStyles(styles);
+
+export default function Main(props) {
+  const classes = useStyles();
+  const { ...rest } = props;
+  return (
+    <div>
+      <Header
+        brand="Digital Beans"
+        rightLinks={<HeaderLinks />}
+        fixed
+        color="dark"
+        {...rest}
+      />
+      <Parallax>
+        <div className={classes.container}>
+          <GridContainer>
+            <GridItem>
+              <div className={classes.brand}>
+                <h1 className={classes.title}>Digital Beans</h1>
+                <h3 className={classes.subtitle}>
+                  Yet Another Tech Blog
+                </h3>
+              </div>
+            </GridItem>
+          </GridContainer>
+        </div>
+      </Parallax>
+
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <SectionCompletedExamples />
+        <SectionLogin />
+        <GridItem md={12} className={classes.textCenter}>
+          <Link href="/login">
+            <a className={classes.link}>
+              <Button color="primary" size="lg" simple>
+                View Login Page
+              </Button>
+            </a>
+          </Link>
+        </GridItem>
+        <SectionExamples />
+        <SectionDownload />
+      </div>
+      <Footer />
+    </div>
+  );
+}
